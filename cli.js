3267409572007e07f1db7e618e35cdee3b8fb000
isaacs <i@izs.me>
2010-10-05 11:43:04 -0700

    Move the sudon't warning

--- cli.js
+++ cli.js
@@ -76,6 +76,11 @@ else {
   }
   ini.resolveConfigs(conf, function (er) {
     if (er) return errorHandler(er)
+    if (process.getuid() === 0 && command !== "fix-root") {
+      log.error( "\nRunning npm as root is not recommended!\n"
+               + "Use `sudo npm fix-root` to fix this.\n"
+               , "sudon't!")
+    }
     npm.config.set("root", ini.get("root"))
     npm.commands[command](arglist, errorHandler)
   })
